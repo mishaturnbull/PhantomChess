ddf0b0d896a9c69ad7d967632316497e5215d4e1
cclauss <cclauss@bluewin.ch>
2015-04-20 13:21:27 +0200

    Try printing a unicode message and see if it succeeds

--- Phantom/can_unicode.py
+++ Phantom/can_unicode.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 #########################################################################
 # This file is part of PhantomChess.                                    #
@@ -17,23 +18,12 @@
 # along with PhantomChess.  If not, see <http://www.gnu.org/licenses/>. #
 #########################################################################
 
-"""Deterime which platform you are using, and decide whether it supports
-(successful) use of unicode characters and defaults to a monospaced font."""
+"""Deterime your platform supports printing of unicode characters."""
 
-import platform
-
-# The default terminal on Windows currently has two problems:
-# 1) It does not support Unicode!
-# 2) It can not be configured to default to a monospaced font
-
-unicode_systems_dict = {
-    'Darwin'  : True,    # Mac OSX and Pythonista for iOS
-    'Java'    : True,    # Jython
-    'Linux'   : True,    # Works like Darwin
-    'Windows' : False }  # Lacks Unicode support
-
-def can_unicode():
-    return unicode_systems_dict.get(platform.system(), False)
-
-if __name__ == '__main__':
-    print('can_unicode(): {}'.format(can_unicode()))
+def use_unicode(s='Welcome to PhantomChess...'):
+    try:
+        print('♜ ♞ ♝  {} ♗ ♘ ♖ '.format(s))
+        return True
+    except UnicodeEncodeError:
+        print(str(s))
+        return False
