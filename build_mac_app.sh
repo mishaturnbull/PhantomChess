c3081b5255d4a35166c0e457d22453adb4a6db66
cclauss <cclauss@bluewin.ch>
2015-04-20 13:44:41 +0200

    Create build_mac_app.sh
    Creates an Mac OSX 64-bit app that currently does NOT support unicode printing so it uses ascii instead.

---
+++ build_mac_app.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# required that you have done `pip install pyinstaller`
+pyinstaller --onefile Phantom/Run_this.py -n Phantom_for_MacOSX_64bit
+echo "Phantom_for_MacOSX_64bit has been built in Phantom/dist:"
+ls -la Phantom/dist
+open Phantom/dist
