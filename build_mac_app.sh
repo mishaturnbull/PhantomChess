5d6535bd800e793fa07cae4be659bb2e2d1aad36
cclauss <cclauss@bluewin.ch>
2015-04-22 11:02:47 +0200

    Update build_mac_app.sh

--- build_mac_app.sh
+++ build_mac_app.sh
@@ -2,5 +2,5 @@
 # requires that you have already done `pip install pyinstaller`
 pyinstaller --onefile Phantom/Run_this.py -n Phantom_for_MacOSX_64bit
 echo "Phantom_for_MacOSX_64bit has been built in Phantom/dist:"
-ls -la Phantom/dist  # display file size, etc. of new app
-open Phantom/dist    # show the new app in a Finder window
+ls -la dist  # display file size, etc. of new app
+open dist    # show the new app in a Finder window
