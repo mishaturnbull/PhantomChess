80f6569ffda73541be6dbb8aa8f35905a4f66b02
cclauss <cclauss@bluewin.ch>
2015-04-22 12:26:01 +0200

    From __future__ import

--- Phantom/utils/tests/run_tests.py
+++ Phantom/utils/tests/run_tests.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 #########################################################################
 # This file is part of PhantomChess.                                    #
@@ -40,4 +42,3 @@ def main(*args):
 
 if __name__ == '__main__':
     main()
-
