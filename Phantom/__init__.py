14378cfb395f0a8408ad6c73b4dcaac3c850f855
cclauss <cclauss@bluewin.ch>
2015-04-23 19:22:10 +0200

    Removing relative imports to reenable Python commandline play and also run_tests.py

--- Phantom/__init__.py
+++ Phantom/__init__.py
@@ -25,10 +25,10 @@ The module IS `import *` safe.  (It's still not a good habit though.)"""
 from Phantom.core.coord.point import Coord, Grid, bounds
 from Phantom.core.coord.dirs import dirfinder
 from Phantom.core.coord.vectored_lists import north, south, east, west, ne, se, sw, nw
-import core.coord.point as point
-import core.coord.dirs as dirs
-import core.coord.vectored_lists as vectored_lists
-import core.coord as coord
+#import core.coord.point as point
+#import core.coord.dirs as dirs
+#import core.coord.vectored_lists as vectored_lists
+#import core.coord as coord
 
 from Phantom.core.board import load, Tile, Board
 from Phantom.core.game_class import loadgame, ChessGame
@@ -36,37 +36,37 @@ from Phantom.core.pieces import ChessPiece, Pawn, Bishop, Knight, Rook, Queen, K
 from Phantom.core.chessobj import PhantomObj
 from Phantom.core.exceptions import ChessError, LogicError, InvalidMove, InvalidDimension
 from Phantom.core.players import Side, Player
-import core.board as board
-import core.game_class as game_class
-import core.pieces as pieces
-import core.chessobj as chessobj
-import core.exceptions as exceptions
-import core.players as players
-import core as core
+#import core.board as board
+#import core.game_class as game_class
+#import core.pieces as pieces
+#import core.chessobj as chessobj
+#import core.exceptions as exceptions
+#import core.players as players
+#import core as core
 
 from Phantom.boardio.load import listgames, loadgame
 from Phantom.boardio.save import save
 from Phantom.boardio.boardcfg import Cfg, Namespace
 from Phantom.boardio.epd_read import load_epd, load_test, load_test_string
 #import boardio.load as load  # conflicts with Phantom.core.board.load
-import boardio.save as save
-import boardio.boardcfg as boardcfg
-import boardio.epd_read as epd_read
+#import boardio.save as save
+#import boardio.boardcfg as boardcfg
+#import boardio.epd_read as epd_read
 
 from Phantom.utils.debug import log_msg, call_trace, clear_log
 from Phantom.utils.decorators import named, exc_catch, integer_args, default_args
 from Phantom.utils.lic import short, license
 from Phantom.utils.timer import Timer
-import utils.debug as debug
-import utils.decorators as decorators
-import utils.lic as lic
-import utils.timer as timer
+#import utils.debug as debug
+#import utils.decorators as decorators
+#import utils.lic as lic
+#import utils.timer as timer
 
 #from Phantom.constants import *
-import Phantom.constants as constants
+#import Phantom.constants as constants
 
 from Phantom.functions import dist, round_down, round_up
-import Phantom.functions as functions
+#import Phantom.functions as functions
 
 from Phantom.__version__ import (__major__, __minor__, __patch__, __version__, __author__,
                                  __author_email__, __license__, __last_update__)
