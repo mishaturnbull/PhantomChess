674e6d50571eb28a775ff0297be6d873df09ed64
cclauss <cclauss@bluewin.ch>
2015-04-23 17:12:43 +0200

    Deal with the strange case where log_msg == None
    See the TypeError mentioned in issue https://github.com/671620616/PhantomChess/issues/48

--- Phantom/core/chessobj.py
+++ Phantom/core/chessobj.py
@@ -35,17 +35,15 @@ class PhantomObj (object):
         log_msg('created object of type {}'.format(cls.__name__), 10)
 
         # keep track of number of instances of each class
-        if cls not in class_record:
-            class_record.update({cls: 1})
-        else:
-            class_record[cls] += 1
+        class_record[cls] = class_record.get(cls, 0) + 1
         return inst
 
     def __repr__(self):
         return "<{}.{} object at {}>".format(__name__, self.__class__.__name__, hex(id(self)))
 
     def __del__(self):
-        log_msg('object of type {} deleted'.format(self.__class__.__name__), 10)
+        if log_msg:  # ccc: it is unclear to me why log_msg == None
+            log_msg('object of type {} deleted'.format(self.__class__.__name__), 10)
         class_record[self.__class__] -= 1
 
     def __eq__(self, other):
