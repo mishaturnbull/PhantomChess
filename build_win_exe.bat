78d00266426af551bf9230adbf7f6a141d717227
671620616 <mishaturnbull@gmail.com>
2015-04-24 18:04:02 -0400

    Improve build_win_exe.bat
    Improved the batch file to clean up afterwards and a rebuild exe.

--- build_win_exe.bat
+++ build_win_exe.bat
@@ -1,2 +1,8 @@
 # done ~~todo: 671620616~~
-pyinstaller Phantom\Run_this.py --onefile -n Simple.exe
+echo Building Simple.exe..
+pyinstaller Phantom\Run_this.py -F -n Simple
+echo Done, cleaning up...
+move /Y Phantom\Simple.exe Simple.exe
+rd /S /Q dist
+rd /S /Q build
+echo Complete!
