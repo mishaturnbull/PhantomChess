a6f3a3dad96d59be3d4ae6753a3c6839c06d7222
cclauss <cclauss@bluewin.ch>
2015-04-27 16:51:46 +0200

    Properly center LoadingScreen text on iPhone screens

--- Phantom/core/game_class.py
+++ Phantom/core/game_class.py
@@ -131,12 +131,13 @@ class ChessGame (PhantomObj):
             from Phantom.gui_pythonista.screen_options import ChessOptionsScreen
             from Phantom.gui_pythonista.screen_promote import ChessPromoteScreen
             self.data['screen_main'] = ChessMainScreen(self)
-            self.data['screen_load'] = ChessLoadingScreen()
+            #self.data['screen_load'] = ChessLoadingScreen()
             self.data['screen_options'] = ChessOptionsScreen(self)
             self.data['screen_promote'] = ChessPromoteScreen(self)
             self.data['main_scene'] = MultiScene(self.data['screen_load'])
+            self.data['screen_load'] = ChessLoadingScreen(self.data['main_scene'])
             self.data['screen_main'].set_parent(self.data['main_scene'])
-            self.data['screen_load'].set_parent(self.data['main_scene'])
+            #self.data['screen_load'].parent = self.data['main_scene']
             self.data['screen_options'].set_parent(self.data['main_scene'])
             self.data['screen_promote'].set_parent(self.data['main_scene'])
             self.data['main_scene'].switch_scene(self.data['screen_load'])
