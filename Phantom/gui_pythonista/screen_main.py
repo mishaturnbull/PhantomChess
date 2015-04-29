e12bceed458126c4abe1a601f75389cf91d1bc61
cclauss <cclauss@bluewin.ch>
2015-04-29 23:52:57 +0200

    Move to scene.SceneView

--- Phantom/gui_pythonista/screen_main.py
+++ Phantom/gui_pythonista/screen_main.py
@@ -6,12 +6,12 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 # This file is part of PhantomChess.                                    #
 #                                                                       #
 # PhantomChess is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU General Public License as published by  # 
+# it under the terms of the GNU General Public License as published by  #
 # the Free Software Foundation, either version 3 of the License, or     #
 # (at your option) any later version.                                   #
 #                                                                       #
 # PhantomChess is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        # 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
 # GNU General Public License for more details.                          #
 #                                                                       #
@@ -31,53 +31,42 @@ from Phantom.utils.debug import log_msg, call_trace
 from Phantom.constants import *
 from Phantom.ai.pos_eval.advanced import pos_eval_advanced
 from Phantom.core.exceptions import InvalidMove
+from Phantom.gui_pythonista.screen_options import ChessOptionsScreen
+from Phantom.gui_pythonista.screen_promote import ChessPromoteScreen
+
+def load_images():
+    folder = 'imgs'
+    fmt = 'Chess set images {} {}.jpg'
+    filenames = [os.path.join(phantom_dir, 'gui_pythonista', folder, fmt.format(color, type))
+                 for type in 'pawn rook queen king bishop knight'.split()
+                 for color in ('black', 'white')]
+    return {os.path.split(filename)[1]:scene.load_image_file(filename)
+                 for filename in filenames}
+
 
 class ChessMainScreen (scene.Scene, PhantomObj):
-    
-    def __init__(self, game, parent=None):
-        self.game = game
-        self.parent = parent  # Phantom.gui_pythonista.main_scene.Multiscene.Scene object
+
+    def __init__(self, game_view):
+        self.game_view = game_view
         self.tmp_t = 0
 
-
-    @property
-    def parent(self):
-        return self._parent
-
-    @parent.setter
-    def parent(self, parent):
-        self._parent = parent
-        if self.parent:
-            self.parent.set_main_scene(self)
-
     def setup(self):
-        self.coord_disp_mode = {'onoff': self.game.board.cfg.disp_coords,
-                                'mode': self.game.board.cfg.coord_mode}
-        self.render_mode = {'sqrs': self.game.board.cfg.disp_sqrs,
-                            'pieces': self.game.board.cfg.disp_pieces,
-                            'valid': self.game.board.cfg.highlight,
-                            'coords': self.game.board.cfg.disp_coords,
-                            'turn': self.game.board.cfg.disp_turn,
-                            'timers': self.game.board.cfg.disp_timers}
+        cfg = self.game_view.game.board.cfg
+        self.coord_disp_mode = {'onoff': cfg.disp_coords,
+                                'mode': cfg.coord_mode}
+        self.render_mode = {'sqrs': cfg.disp_sqrs,
+                            'pieces': cfg.disp_pieces,
+                            'valid': cfg.highlight,
+                            'coords': cfg.disp_coords,
+                            'turn': cfg.disp_turn,
+                            'timers': cfg.disp_timers}
         self.is_selected = False
         self.selected = Coord(None, None)
         self.target = self.selected
         self.err = None
         self.err_pos = Coord(None, None)
         self.valid_cache = []
-        folder = 'imgs'
-        format = 'Chess set images {} {}.jpg'
-
-        files = [os.path.join(phantom_dir, 'gui_pythonista', folder, format.format(color, type))
-                 for type in ('pawn', 'rook', 'queen', 'king', 'bishop', 'knight')
-                 for color in ('black', 'white')]
-
-        img_names = {}
-        for file in files:
-            name = os.path.split(file)[1]
-            img = scene.load_image_file(file)
-            img_names.update({name: img})
-        self.img_names = img_names
+        self.img_names = load_images()
         self.turn_indicator_img = 'White_Square'
         self.pos_score = None
         self.disp_score = False
@@ -85,25 +74,26 @@ class ChessMainScreen (scene.Scene, PhantomObj):
         max = Coord(8, 8).as_screen
         self.bounds = scene.Rect(min.x, min.y, max.x-min.x, max.y-min.y)
         self.size = screen_size
-        self.won = self.game.is_won()
-    
+        self.won = self.game_view.game.is_won()
+
     def did_err(self, e):
         self.err = sys.exc_info()
         self.selected = Coord(None, None)
         self.is_selected = False
-        self.game.board.freeze()
-    
+        self.game_view.game.board.freeze()
+
     def touch_began(self, touch):
+        game = self.game_view.game
         self.valid_cache = []
         tpos = Coord(touch.location.x, touch.location.y)
         cpos = Coord.from_screen(tpos)
         if touch.location in self.bounds:
             if not self.is_selected:
-                piece = self.game.board[cpos]
+                piece = game.board[cpos]
                 if isinstance(piece, list):
                     try:
-                        raise ChessError('More than one piece at position', 
-                                         'Phantom.gui_pythonista.screen_main.ChessMainScreen.touch_began()')
+                        raise ChessError('More than one piece at position',
+                              'Phantom.gui_pythonista.screen_main.ChessMainScreen.touch_began()')
                     except ChessError as e:
                         self.did_err(e)
                         return
@@ -116,25 +106,26 @@ class ChessMainScreen (scene.Scene, PhantomObj):
             else:
                 self.target = cpos
                 try:
-                    if self.game.board[self.selected].ptype == 'king':
-                        piece = self.game.board[self.selected]
+                    if game.board[self.selected].ptype == 'king':
+                        piece = game.board[self.selected]
+                        castling_rights = game.board.castling_rights
                         if piece.color == 'white':
                             if self.target == Coord(6, 0):
-                                if 'K' in self.game.board.castling_rights:
-                                    self.game.castle('K')
+                                if 'K' in castling_rights:
+                                    game.castle('K')
                             elif self.target == Coord(2, 0):
-                                if 'Q' in self.game.board.castling_rights:
-                                    self.game.castle('Q')
+                                if 'Q' in castling_rights:
+                                    game.castle('Q')
                         elif piece.color == 'black':
                             if self.target == Coord(6, 7):
-                                if 'k' in self.game.board.castling_rights:
-                                    self.game.castle('k')
+                                if 'k' in castling_rights:
+                                    game.castle('k')
                             elif self.target == Coord(2, 7):
-                                if 'q' in self.game.board.castling_rights:
-                                    self.game.castle('q')
-                    mr = self.game.move(self.selected, self.target)
+                                if 'q' in castling_rights:
+                                    game.castle('q')
+                    mr = game.move(self.selected, self.target)
                     self.disp_score = False
-                    self.won = self.game.is_won()
+                    self.won = game.is_won()
                 except InvalidMove as e:
                     self.err_pos = self.target
                     self.did_err(e)
@@ -145,30 +136,31 @@ class ChessMainScreen (scene.Scene, PhantomObj):
                 self.is_selected = False
                 if isinstance(mr, str):
                     self.err_pos = self.target
-                if any([p.is_promotable for p in self.game.board.pieces]):
-                    self.parent.switch_scene(self.game.data['screen_promote'])
+                if any([p.is_promotable for p in game.board.pieces]):
+                    self.game_view.switch_scene(ChessPromoteScreen)
         elif 0 < touch.location.x <= scale_factor:
             if 6*scale_factor < touch.location.y <= 7*scale_factor:
-                self.parent.switch_scene(self.game.data['screen_options'])
-            if 5*scale_factor < touch.location.y <= 6*scale_factor:
-                self.game.ai_easy()
-            if 4*scale_factor < touch.location.y <= 5*scale_factor:
-                self.game.ai_hard()
-            if 3*scale_factor < touch.location.y <= 4*scale_factor:
-                self.pos_score = pos_eval_advanced(self.game.board)
+                self.game_view.switch_scene(ChessOptionsScreen)
+            elif 5*scale_factor < touch.location.y:
+                game.ai_easy()
+            elif 4*scale_factor < touch.location.y:
+                game.ai_hard()
+            elif 3*scale_factor < touch.location.y:
+                self.pos_score = pos_eval_advanced(game.board)
                 self.disp_score = True
-            if 2*scale_factor < touch.location.y <= 3*scale_factor:
-                self.game.rollback()
-            if scale_factor < touch.location.y <= 2*scale_factor:
+            elif 2*scale_factor < touch.location.y:
+                game.rollback()
+            elif scale_factor < touch.location.y:
                 self.is_selected = False
                 self.selected = Coord(None, None)
                 self.target = Coord(None, None)
-    
+
     def draw(self):
         scene.background(0, 0, 0)
         scene.fill(1, 1, 1, 1)
+        board = self.game_view.game.board
         if self.render_mode['pieces']:
-            for piece in self.game.board.pieces:
+            for piece in board.pieces:
                 scene.tint(1, 1, 1, 0.5)
                 pos = piece.coord.as_screen
                 img = self.img_names[piece.pythonista_gui_imgname]
@@ -179,8 +171,8 @@ class ChessMainScreen (scene.Scene, PhantomObj):
                     scene.rect(pos.x, pos.y, scale_factor, scale_factor)
                     scene.fill(1, 1, 1, 1)
             if self.render_mode['timers']:
-                white = str(self.game.board.player1.timer.get_run())
-                black = str(self.game.board.player2.timer.get_run())
+                white = str(board.player1.timer.get_run())
+                black = str(board.player2.timer.get_run())
                 bpos = Coord(992, 672)
                 wpos = Coord(992, 96)
                 scene.tint(1, 1, 1, 1)
@@ -188,7 +180,7 @@ class ChessMainScreen (scene.Scene, PhantomObj):
                 scene.text(white, x=wpos.x, y=wpos.y)
                 scene.tint(1, 1, 1, 1)
         if self.render_mode['sqrs']:
-            for tile in self.game.board.tiles:
+            for tile in board.tiles:
                 color = tile.color.tilecolor
                 color += (0.57,)  # alpha value
                 pos = tile.coord.as_screen
@@ -212,16 +204,16 @@ class ChessMainScreen (scene.Scene, PhantomObj):
                 scene.text('Move\nInvalid', x=(sc.x + scale_factor/2), y=(sc.y + scale_factor/2))
                 scene.tint(1, 1, 1, 1)
         if self.render_mode['valid']:
-            for tile in self.game.board.tiles:
+            for tile in board.tiles:
                 if tile.coord in self.valid_cache:
                     pos = tile.coord.as_screen
                     scene.fill(0.47934,0.81198,0.41839, 0.3)
                     scene.rect(pos.x, pos.y, scale_factor, scale_factor)
-                    scene.fill(1, 1, 1, 1) 
+                    scene.fill(1, 1, 1, 1)
         if self.render_mode['turn']:
-            if self.game.board.turn == 'white':
+            if board.turn == 'white':
                 turn_y = 1 * scale_factor
-            elif self.game.board.turn == 'black':
+            elif board.turn == 'black':
                 turn_y = 7 * scale_factor
             pos = Coord(896, turn_y)
             size = Coord(scale_factor / 2, scale_factor / 2)
@@ -231,23 +223,24 @@ class ChessMainScreen (scene.Scene, PhantomObj):
         if self.won:
             pos = Coord(self.size.w/2, self.size.h/2)
             scene.tint(0.32645,0.28306,0.93492)
-            # commented out until the bug in ChessGame.is_won() is fixed to be 
+            # commented out until the bug in ChessGame.is_won() is fixed to be
             # less annoying
             scene.text('{} wins'.format(self.won), x=pos.x, y=pos.y, font_size=40.0)
             scene.tint(1, 1, 1, 1)
-        
+
         # Buttons
-        scene.text('AI Easy', x=scale_factor/2, y=scale_factor*6 - scale_factor/2)
-        scene.text('AI Hard', x=scale_factor/2, y=scale_factor*5 - scale_factor/2)
-        scene.text('Get score', x=scale_factor/2, y=scale_factor*4 - scale_factor/2)
+        x=scale_factor/2
+        scene.text('AI Easy', x=x, y=scale_factor*6 - x)
+        scene.text('AI Hard', x=x, y=scale_factor*5 - x)
+        scene.text('Get score', x=x, y=scale_factor*4 - x)
         if self.disp_score:
-            scene.text(str(self.pos_score), x=scale_factor/2, y=scale_factor*4 - scale_factor/1.5)
-        scene.text('Undo', x=scale_factor/2, y=scale_factor*3 - scale_factor/2)
-        scene.text('Deselect', x=scale_factor/2, y=scale_factor*2 - scale_factor/2)
-        scene.text('Options', x=scale_factor/2, y=scale_factor*7 - scale_factor/2)
+            scene.text(str(self.pos_score), x=x, y=scale_factor*4 - scale_factor/1.5)
+        scene.text('Undo', x=x, y=scale_factor*3 - x)
+        scene.text('Deselect', x=x, y=scale_factor*2 - x)
+        scene.text('Options', x=x, y=scale_factor*7 - x)
 
 if __name__ == '__main__':
-    from Phantom.core.game_class import ChessGame, load_game
+    from Phantom.core.game_class import ChessGame, loadgame
     game = ChessGame() #loadgame('Long Endgame 1')
     game.board.cfg.disp_sqrs = True
     s = ChessMainScreen(game)
