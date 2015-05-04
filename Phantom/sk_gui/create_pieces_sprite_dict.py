35b68d0ccba663a5d898c5c6a3931e82d4ba8484
cclauss <cclauss@bluewin.ch>
2015-05-04 10:10:50 +0200

    Reworked create_board_tiles_dict()
    Now we have to make the pieces drag and drop to board squares.

--- Phantom/sk_gui/create_pieces_sprite_dict.py
+++ Phantom/sk_gui/create_pieces_sprite_dict.py
@@ -1,29 +1,31 @@
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 import os, sk
 from Phantom.core.coord.point import Coord
+from Phantom.core.game_class import ChessGame
 import Phantom.constants as C
 
 img_dir = '../gui_pythonista/imgs'
 
-class Game (sk.Scene):
+class GameScene(sk.Scene):
     def __init__(self, game):
         sk.Scene.__init__(self)
         self.game = game
         self.name = 'GameNode'
         chess_pieces_dict = self.create_pieces_sprite_dict()
-        background_tile_dict = self.create_background_tiles_dict()
         for i, piece_name in enumerate(sorted(chess_pieces_dict)):
             node = chess_pieces_dict[piece_name]
             node.name = piece_name
             node.position += ((i+1) * 80, 20 + (i+1) * 50)
             node.alpha = 0.5
             self.add_child(node)
-        for pos in background_tile_dict:
-            self.add_child(background_tile_dict[pos])
+        board_tiles_dict = self.create_board_tiles_dict()
+        for pos in self.create_board_tiles_dict():
+            self.add_child(board_tiles_dict[pos])
         for piece in self.get_children_with_name('*'):
             print(piece.name, piece.frame)
-
         self.selected = None
         self.target_pos = None
         self.selected_pos = None
@@ -38,21 +40,22 @@ class Game (sk.Scene):
         fmt = os.path.join(img_dir, 'Chess set images {}.jpg')
         return {name:sk.SpriteNode(sk.Texture(fmt.format(name))) for name in short_names}
 
-    @classmethod
-    def create_background_tiles_dict(cls):
+    def create_board_tiles_dict(self):
         # return a dictionary of {Phantom.core.coord.point.Coord :
-        #                          colored_square_as_sk.Spritenode}
+        #                          colored_square_as_sk.SpriteNode}
         # useful for node hit-testing,much simplifies touch_began
-        ret = {}
-        size = sk.Size(C.scale_factor, C.scale_factor)
-        for tile in self.game.board.tiles:
+        tile_size = sk.Size(C.scale_factor, C.scale_factor)
+        def make_board_tile(tile):
             node = sk.SpriteNode()
-            node.size = size
+            node.name = str(tile.coord.as_chess)
+            node.size = tile_size
             node.alpha = 0.3
-            node.position = tile.coord.as_screen()
+            pos = tile.coord.as_screen
+            node.position = (pos.x, pos.y)
             node.color = tile.tilecolor
-            ret.update({tile.coord: node})
-        return ret
+            return node
+        return {tile.coord : make_board_tile(tile)
+                for tile in self.game.board.tiles}
 
     def did_change_size(self, old_size):
         pass
@@ -71,8 +74,9 @@ class Game (sk.Scene):
 
 def main():
     print('=' * 30)
-    game = Game()
-    scene_view = sk.View(game)
+    game = ChessGame()
+    game_scene = GameScene(game)
+    scene_view = sk.View(game_scene)
     scene_view.shows_fps = True
     scene_view.present()
 
