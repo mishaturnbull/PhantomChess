90b0a471a6f294d8c400da3cc07479ca7a2e7aed
671620616 <mishaturnbull@gmail.com>
2015-05-08 19:18:35 -0400

    Readd Simple.exe Fix build_win_exe.bat

--- build_win_exe.bat
+++ build_win_exe.bat
@@ -1,8 +1,8 @@
 # done ~~todo: 671620616~~
 echo Building Simple.exe..
-pyinstaller Phantom\Run_this.py -F -n Simple
+pyinstaller Phantom\Run_this.py -F
 echo Done, cleaning up...
-move /Y Phantom\Simple.exe Simple.exe
+move /Y dist\Run_this.exe Simple.exe
 rd /S /Q dist
 rd /S /Q build
 DEL /Q Simple.spec
