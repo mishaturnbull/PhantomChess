a0f088ee65fb4280ac64a71c4bcda705b0990ad1
cclauss <cclauss@bluewin.ch>
2015-05-22 19:31:43 +0200

    Background image, hud_alerts, etc.

--- Phantom/sk_gui/SkChessView.py
+++ Phantom/sk_gui/SkChessView.py
@@ -20,15 +20,16 @@
 
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
-import sk, sys, ui
+import dialogs, photos, sk, sys, ui
 #import SkChessBoardScene ; reload(SkChessBoardScene)  #Pythonista workaround
 from Phantom.sk_gui.SkChessBoardScene import SkChessBoardScene
 from Phantom.core.game_class import ChessGame
 
 def quit_action(sender):
-    msg = '{} not implemented!!'.format(sender.title)
+    msg = '{} is not yet implemented!!'.format(sender.title)
+    dialogs.hud_alert(msg)
     print(msg)
-    sys.exit(msg)
+    #sys.exit(msg)
 
 def center_square():
     w, h = ui.get_screen_size()
@@ -39,10 +40,18 @@ def center_square():
 
 class SkChessView(ui.View):
     def __init__(self, game=None):
+        self.width, self.height = ui.get_screen_size()
+        if photos.is_authorized():
+            self.add_subview(self.make_image_view())
         game = game or ChessGame()
         board_scene = self.make_board_scene(game)
         self.make_buttons()
         self.present(orientations=['landscape'], hide_title_bar=True)
+
+    def make_image_view(self, image_name=''):
+        image_view = ui.ImageView(frame=self.bounds)
+        image_view.image = ui.Image.from_data(photos.get_image(raw_data=True)) # ui.Image('emj:Smiling_1')
+        return image_view
 
     def make_board_scene(self, game):
         board_scene = SkChessBoardScene(game)
