90564f0e8db6ee15ed8304d954f8f3c8b49a798d
cclauss <cclauss@bluewin.ch>
2015-05-23 22:52:42 +0200

    Fix AI after refactor

--- Phantom/utils/tests/movement.py
+++ Phantom/utils/tests/movement.py
@@ -21,13 +21,15 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 """Test the Phantom.core.board.Board.move() method"""
 
-from Phantom.core.board import Board
+#from Phantom.core.board import Board
+from Phantom.core.game_class import ChessGame
 from Phantom.utils.debug import log_msg, clear_log
 
 def main(clear=True):
     if clear: clear_log()
     log_msg('Testing Phantom.core.board.Board.move() method', 0)
-    b = Board()  # white to move, opening layout
+    #b = Board(None)  # white to move, opening layout
+    b = ChessGame().board
     try:
         b.move('e2e4')
         b.move('g8f6')
