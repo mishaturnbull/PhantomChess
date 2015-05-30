db47468df12b27055393dec4e3e732719650e6bf
cclauss <cclauss@bluewin.ch>
2015-05-30 23:03:13 +0200

    Consolidate can_print_unicode()
    Based on learnings from http://omz-forums.appspot.com/pythonista/post/5822990663024640
    
    Thanks @dgelessus

--- Phantom/can_print_unicode.py
+++ Phantom/can_print_unicode.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division, print_function, unicode_literals)
-import os, sys
 
 #########################################################################
 # This file is part of PhantomChess.                                    #
@@ -21,44 +20,14 @@ import os, sys
 
 """Deterime your platform supports printing of unicode characters."""
 
-fmt = '''
-    PYTHONIOENCODING:    {}
-    sys.stdout.encoding: {}
-    sys.stderr.encoding: {}'''
+import sys
 
-def can_print_unicode(s='Welcome to PhantomChess...'):
-    # python -c "import sys ; print(sys.stderr.encoding)"
-    # 671's Windows box defaults to cp437 but a buddy in Germany's box defaults to cp850
-
-    # 671: turns out that cp437 is also known as MS-DOS, the set of printable
-    #      characters in a Windows command prompt.  cp850 is for our purposes
-    #      identical to cp437 (it differs in having Greek chars with accents I
-    #      believe).  However, neither cp437 nor cp850 support the chess glyphs
-    #      that are needed in Phantom.  It seems to me that utf-8 encoding in
-    #      PyInstaller would be unlikely, as it requires a font that is **not**
-    #      supported by Windows in the command prompt windows that Simple.exe
-    #      opens in.  For now, I would recommend using SAN characters.
-    #      http://en.wikipedia.org/wiki/Code_page_850
-    #      http://en.wikipedia.org/wiki/Code_page_437
-    #      http://en.wikipedia.org/wiki/UTF-8
-
-    #print('Before: ' + fmt.format(os.getenv('PYTHONIOENCODING', None),
-    #                             sys.stdout.encoding,
-    #                             sys.stderr.encoding))
-    default_encoding = (sys.stdout.encoding
-                        or ('cp437' if sys.platform.startswith('win') else 'utf-8'))
-    if not os.getenv('PYTHONIOENCODING', None):  # PyInstaller workaround
-        os.environ['PYTHONIOENCODING'] = default_encoding
+def can_print_unicode(msg='Welcome to PhantomChess...'):
     try:
-        print(u'♜ ♞ ♝ {} ♗ ♘ ♖ '.format(s).encode(default_encoding))
-        #print('After: ' + fmt.format(os.getenv('PYTHONIOENCODING', None),
-        #                             sys.stdout.encoding,
-        #                             sys.stderr.encoding))
-        # 671: is default_encoding is derived from sys.stdout, so it will always be
-        #      equal to it, hence this is always True
-        return (sys.stdout.encoding == default_encoding.upper())  # True
+        print(str(u'♜ ♞ ♝ {} ♗ ♘ ♖ '.format(msg)))
+        return True
     except UnicodeEncodeError:
-        print(str(s))
+        print(msg)
         return False
 
 if __name__ == '__main__':
